48a719e15b7b4275ae90ae93f8920e89270203cd
jingwei218 <yuanjingwei@aliyun.com>
2016-02-21 12:25:40 +0800

    combinations complete

--- PortableCalc/PortableCalc.py
+++ PortableCalc/PortableCalc.py
@@ -1,13 +1,32 @@
 ﻿from xlsx import *
 from combination import *
 
+#excel based
 xldata = []
 for col in first_columns:
     xldata.append(xlsxData(start_x=col))
 u = U(xldata)
 
+combo_of_vendors = combinations(u.vendors, nbr_of_selected)
+
 #testing
-ux=u.getInterimAtoms(filterlist=[["Schenker", "DHL"]])
-ud=u.removeRedundancy(ux)
-sp=[x for x in ud if x[3][0]==1]
-#ul = u.getAllCombinations(ud, sp)
+for combo in combo_of_vendors:
+    print(combo)
+    ui = u.getInterimAtoms(filterlist=[combo])
+    print('interim')
+    print(ui)
+    ur = u.removeRedundancy(ui)
+    print('remove redundancy')
+    print(ur)
+    us = u.structureAtoms(ur)
+    print('structured')
+    print(us)
+    start = [a for a in us if a[3][0]==1]
+    u.getAllCombinations(us, start)
+
+x = 1
+print('======= start of combinations =========')
+for item in u.getCombinationList():
+    print(item)
+    print('========= end of combo ' + str(x) + ' ============')
+    x += 1
--- PortableCalc/combination.py
+++ PortableCalc/combination.py
@@ -4,7 +4,6 @@ from copy import deepcopy
 class U:
 
     __atom_combinations = []
-    __index_test = 0
 
     def __init__(self, rawDataObjs=None):
         '''rawDataObjs: a sequence of raw data objects (only include tables to be mixed)
@@ -21,6 +20,7 @@ class U:
         self.nbr_of_rows = len(rawDataObjs[0].table[0]) #number of rows in tables, all tables should contain the same number of rows
         self.atoms = {} #dict {vendor-aggregation: [aggregation_name, aggregation_value, [ids]]}
         self.aggregations = [] #aggregation names
+        self.vendors = [] #vendor names
 
         for k in range(self.nbr_of_mixin):
             for i in range(self.nbr_of_columns):
@@ -31,77 +31,117 @@ class U:
                     id = j + 1 #self.rawDataObjs[k].table[i][j][3] #the kth table, ith column, jth row, 4th item
                     if name not in self.aggregations and name != "U" and value > 0: #aggregation not aggregated yet
                         self.aggregations.append(name)
-                        self.atoms[vendor + "-" + name] = [vendor, name, value, [id]]
+                        self.atoms[vendor + "-" + name] = [vendor, name, value, [id]] #atom[0][1][2][3]
                     elif name in self.aggregations and name != "U" and value == 0: #aggregation aggregated
                         self.atoms[vendor + "-" + name][3].append(id)
                     elif name not in self.aggregations and name == "U" and value > 0: #single items
                         self.atoms[vendor + "-" + name + "-" + str(id)] = [vendor, name, value, [id]]
+                    if vendor not in self.vendors:
+                        self.vendors.append(vendor)
 
     def getInterimAtoms(self, filterby=[0], filterlist=None):
         '''get sorted interim atoms list
            filter by xth item in atom
            filterlist should map atom [[filterby0], [filterby1], ...]
         '''
-        self.__atoms_interim = [] #sorted list of aggregations derived from atoms dict [[vendor name, aggregation name, value, aggregation], ...]
+        _atoms_interim = [] #sorted list of aggregations derived from atoms dict [[vendor name, aggregation name, value, aggregation], ...]
         for atom in self.atoms:
             _bfilter = True #boolean flag of filter
             for fb in filterby:
                 _bfilter = _bfilter and self.atoms[atom][fb] in filterlist[fb] #item in atoms[atom][x] also in filterlist[x]
             if _bfilter:
-                self.__atoms_interim.append(self.atoms[atom]) #add atoms to interim list
-        self.__atoms_interim.sort(key=lambda x: x[3]) #sort by 4th item (aggregation)
-        return self.__atoms_interim
+                _atoms_interim.append(self.atoms[atom]) #add atoms to interim list
+        _atoms_interim.sort(key=lambda x: x[3]) #sort by 4th item (aggregation)
+        return _atoms_interim
 
-    def removeRedundancy(self, atomsList, keepMax = False):
+    def removeRedundancy(self, atomList, keepMax = False):
         '''remove duplicates and redundancy in atoms_interim'''
         coef = 1 if keepMax == False else -1 #coefficient used to compare Max or Min
-        self.__atoms_no_duplicate = [] #take out redundancy leaving max or min value of same shapes
-        tempAtom = atomsList[0] #initialise temp atom list
-        for atom in atomsList:
+        _atoms_no_duplicate = [] #take out redundancy leaving max or min value of same shapes
+        tempAtom = atomList[0] #initialise temp atom
+        for atom in atomList:
             tempVal = atom[2] #value of atom
             tempAggregation = atom[3] #aggregation of atom
-            tempName = atom[0] #vendor name
             if tempAggregation == tempAtom[3]: #if in same shape
                 if coef*tempVal < coef*tempAtom[2]: #compare
                     tempAtom = atom
             else:
-                self.__atoms_no_duplicate.append(tempAtom) #append atom of last aggregation
+                _atoms_no_duplicate.append(tempAtom) #append atom of last aggregation before current one
                 tempAtom = atom #switch to new aggregation
-        self.__atoms_no_duplicate.append(tempAtom) #append last atom
-        
-        self.__atoms_complete = deepcopy(self.__atoms_no_duplicate) #[vendor name, aggregation name, value, aggregation, [links], [[link indices]...]], ...]
-        for atom in self.__atoms_complete:
+        _atoms_no_duplicate.append(tempAtom) #append last atom
+        return _atoms_no_duplicate
+
+    def structureAtoms(self, atomList): #[vendor name, aggregation name, value, aggregation, [links], [[link indices],...]], ...]
+        _atoms_structured = deepcopy(atomList)
+        for atom in _atoms_structured:
             lastId = atom[3][len(atom[3]) - 1] #last id in aggregation
-            atomIndex = self.__atoms_complete.index(atom) #index of atom in __atoms_complete
+            atomIndex = _atoms_structured.index(atom) #index of atom in _atoms_structured
+            atomsAfter = _atoms_structured[atomIndex+1 : len(_atoms_structured)]
             atom.append(atomIndex) #atom[4]
-            links = list(set(range(atom[3][0], lastId + 1)).difference(set(atom[3]))) #ids not continuous, ids in between become links
+            nextAtomIds = list(set(range(atom[3][0], lastId + 1)).difference(set(atom[3]))) #ids not continuous, ids in between become links
             if lastId < self.nbr_of_rows: #not last atom in column
-                links.append(lastId + 1) #add additional link to links
-            atom.append(links) #atom[5]
-            #linkPoints = []
-            #atomsAfter = self.__atoms_complete[atomIndex+1 : len(self.__atoms_complete)]
-            #if links != []:
-            #    for linkPoint in links:
-            #        tempIndices = []
-            #        for nextAtom in atomsAfter:
-            #            if nextAtom[3][0] == linkPoint:
-            #                linkIndex = self.__atoms_complete.index(nextAtom) #index of linkAtom in _atom_no_duplicate
-            #                tempIndices.append(linkIndex) #indices of atoms that can link this atom
-            #        linkPoints.append(tempIndices)
-            #atom.append(linkPoints) #atom[6]
-        return self.__atoms_complete
+                nextAtomIds.append(lastId + 1) #add additional link after lastId to links
+            atom.append(nextAtomIds) #atom[5] atom ids
+            if nextAtomIds != []: #there is one or more next atoms
+                nextAtomIndices = findNextAtomIndices(atomList, nextAtomIds) #list of nextAtom indices
+            else:
+                nextAtomIndices = []
+            atom.append(nextAtomIndices) #atom[6] atom indices
+        return _atoms_structured
 
     #startAtoms = [x for x in atomList if x[3][0]==1]
-    def getAllCombinations(self, atomList, startAtoms, startComboAtoms=[], startRedLines=[]):
+    def getAllCombinations(self, atomList, startAtoms, startComboAtoms=[], startRedLines=[], startExtraAtoms=[]):
         for atom in startAtoms:
+            bTouchRedLine = False
             comboAtoms = deepcopy(startComboAtoms)
             redLines = deepcopy(startRedLines)
-            comboAtoms.append(atom)
-            if atom[5] != []:
-                newStartAtoms = []
-                for n in atom[5]:
-                    newStartAtoms.append(atomList[n])
-                self.getAllCombinations(atomList, newStartAtoms, comboAtoms)
+            extraAtoms = deepcopy(startExtraAtoms)
+            for aggregation in atom[3]: #go through atom's aggregation
+                if aggregation in redLines:
+                    bTouchRedLine = True #touch red line
+                    break #stop for loop if touch red line
+                else:
+                    redLines.append(aggregation)
+            if bTouchRedLine == True:
+                continue #skip this atom
             else:
-                self.__atom_combinations.append(comboAtoms)
-                startRedLines = []
+                comboAtoms.append(atom)
+            if atom[5] != []: #not the last in column, has next atom to connect
+                newStartAtoms = [] #new startAtoms for next iteration
+                for m in atom[6]: #go through indices of nextAtoms
+                    newStartAtoms.append(atomList[m])
+                for ea in extraAtoms:
+                    if ea[3] in redLines:
+                        extraAtoms.remove(ea)
+                    if ea not in newStartAtoms:
+                        newStartAtoms.append(ea)
+                if len(atom[5]) > 1:
+                    extraStartIds = atom[5][1:len(atom[5])+1]
+                    for n in findNextAtomIndices(atomList, extraStartIds):
+                        if atomList[n] not in extraAtoms:
+                            extraAtoms.append(atomList[n])
+                self.getAllCombinations(atomList, newStartAtoms, comboAtoms, redLines, extraAtoms)
+            else:
+                if isValidCombination(comboAtoms, list(range(1, self.nbr_of_rows+1))):
+                    self.__atom_combinations.append(comboAtoms)
+
+    def getCombinationList(self):
+        return self.__atom_combinations
+
+def findNextAtomIndices(atomList, startIds):
+    _foundAtomIndices = []
+    for atom in atomList:
+        if atom[3][0] in startIds:
+            _foundAtomIndices.append(atomList.index(atom))
+    return _foundAtomIndices
+
+def isValidCombination(atomList, idList):
+    _idList = []
+    for atom in atomList:
+        for id in atom[3]:
+            _idList.append(id)
+    diff = list(set(idList).difference(set(_idList)))
+    if diff == []:
+        return True
+    else:
+        return False
